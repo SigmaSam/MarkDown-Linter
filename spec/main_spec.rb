774c4d0a160a2b3072555d2cced87fc1b48248ce
Samuel <samuelalmaoherrera@gmail.com>
2020-08-31 09:29:05 -0300

    Finished tests for rspec.

---
+++ spec/main_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../lib/mb_scaner.rb'
+
+describe Scaner do
+  let(:file) { File.open('./docs/test.MD', 'r') }
+  let(:file_error) { File.open('./docs/mistakes test.MD', 'r') }
+  let(:scan) { Scaner.new }
+  it 'It returns false when it finds no errors' do
+    expect(scan.scan_line(file)).to be(false)
+  end
+
+  it 'It returns true when it finds errors' do
+    expect(scan.scan_line(file_error)).to be(true)
+  end
+
+  it 'It raises an exception for invalid input' do
+    expect { scan.scan_line('asc') }.to raise_error(NoMethodError)
+  end
+
+  it 'It returns false when it doest not find irregularities' do
+    expect(scan.sym_check(file, 13)).to be(false)
+  end
+
+  it 'It raises an exception for invalid input' do
+    expect { scan.sym_check }.to raise_error
+  end
+
+  it 'It returns false when it finds a symbol without irregularities' do
+    expect(scan.find_pair(file, 33)).to be(false)
+  end
+
+  it 'It raises an exception for invalid input' do
+    expect { scan.find_pair }.to raise_error
+  end
+end
