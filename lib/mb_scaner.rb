3c48069e5219b1376fd6c3e40fb80cd8c85ed06a
Samuel <samuelalmaoherrera@gmail.com>
2020-08-31 02:04:55 -0300

    Fixed Rubocop issues.

--- lib/mb_scaner.rb
+++ lib/mb_scaner.rb
@@ -4,37 +4,30 @@ require 'colorize'
 class Scaner
   def scan_line(file)
     file.readlines.each_with_index do |line, loc|
-      if line.split(' ') != []
-        sym_check(line, loc)
-      end
+      sym_check(line, loc) if line.split(' ') != []
     end
   end
 
   def sym_check(test, ind)
-    if test =~ /[\*\`\[\(]/
-      find_pair(test, ind)
-    end
+    find_pair(test, ind) if test =~ /[\*\`\[\(]/
   end
 
   def find_pair(pair, ubi)
     case pair
     when /\*/
-      puts "You need to close your Asterisks at line #{ubi + 1}".colorize(:background => :red) if pair.count('*').odd?
+      puts "You need to close your Asterisks at line #{ubi + 1}".colorize(background: :red) if pair.count('*').odd?
     when /\`/
-      puts "You need to close your Grave Accent line #{ubi + 1}".colorize(:background => :red) if pair.count('`').odd?
+      puts "You need to close your Grave Accent line #{ubi + 1}".colorize(background: :red) if pair.count('`').odd?
     when /[\[\]]/ && /[\(\)]/
-      puts "You have an irregular number of [ at line #{ubi + 1}".colorize(:background => :red) if pair.count('[').even? && !pair.count('[').zero?
-      puts "You have an irregular number of ] at line #{ubi + 1}".colorize(:background => :red) if pair.count(']').even? && !pair.count(']').zero?
-      puts "You need to close your Square Bracket at line #{ubi + 1}".colorize(:background => :red) if (pair.count('[') + pair.count(']')).odd?
-      puts "You have an irregular number of () at line #{ubi + 1}".colorize(:background => :red) if pair.count('(').even? && !pair.count('(').zero?
-      puts "You have an irregular number of ) at line #{ubi + 1}".colorize(:background => :red) if pair.count(')').even? && !pair.count(')').zero?
-      puts "You need to close your Parenthesis at line #{ubi + 1}".colorize(:background => :red) if (pair.count('(') + pair.count(')')).odd?
+      puts "You have an irregular number of [ at line #{ubi + 1}".colorize(background: :red) if pair.count('[').even? && !pair.count('[').zero?
+      puts "You have an irregular number of ] at line #{ubi + 1}".colorize(background: :red) if pair.count(']').even? && !pair.count(']').zero?
+      puts "You need to close your Square Bracket at line #{ubi + 1}".colorize(background: :red) if (pair.count('[') + pair.count(']')).odd?
+      puts "You have an irregular number of () at line #{ubi + 1}".colorize(background: :red) if pair.count('(').even? && !pair.count('(').zero?
+      puts "You have an irregular number of ) at line #{ubi + 1}".colorize(background: :red) if pair.count(')').even? && !pair.count(')').zero?
+      puts "You need to close your Parenthesis at line #{ubi + 1}".colorize(background: :red) if (pair.count('(') + pair.count(')')).odd?
     end
   end
 end
 
 # rubocop:enable Metrics/AbcSize Metrics/CyclomaticComplexity Metrics/PerceivedComplexity)
 # rubocop:enable  Layout/LineLength Style/HashSyntax
-
-
-
